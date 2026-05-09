89ef23d4010082fc2bca6ec210d63bef6d5b88b7
mbk97 <oyindamola850@gmail.com>
2026-05-09 12:26:50 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // 🔥 VERY IMPORTANT (this fixes Tailwind/CSS/JS paths)
-  base: '/react/',
+ 
 })
